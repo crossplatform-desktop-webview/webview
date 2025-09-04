80633719a723a521f0f88d86ceec2e70125a7bcf
sileanhell <146330319+sileanhell@users.noreply.github.com>
2025-09-04 17:51:41 +0300

    upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "webview/webview.h"
+
+int main() {
+    webview::webview w(false, nullptr);
+    w.set_title("Пример WebView");
+    w.set_size(800, 600, WEBVIEW_HINT_NONE);
+    w.set_html("<h1>Привет из WebView!</h1><p>Это встроенное HTML-окно.</p>");
+    w.run();
+    return 0;
+}
